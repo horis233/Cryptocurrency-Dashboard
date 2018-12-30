73cc7b74ba488da41beae674f5389427ab7d4e53
Horis <horis233@gmail.com>
2018-12-30 15:51:34 -0500

    Test Stlyed component

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
+import styled, {css} from 'styled-components';
+
+const MyButton = styled.div`
+    color: green;
+    ${props => props.primary && css`
+        color: palevioletred;
+    `}
+`
+
+const TomatoButton = styled(MyButton)`
+    color: tomato;
+    board-color: tomato;
+`;
+
 
 class App extends Component {
     render() {
         return ( 
-            <WelcomeMessage/>
+            <div><WelcomeMessage/>
+                <MyButton>Hello</MyButton>
+                <MyButton primary>Hello</MyButton>
+                <TomatoButton primary>Hello</TomatoButton>
+            </div>
         ); 
     }
 }
