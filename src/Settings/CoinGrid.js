056f984743ff1162ec755a891bc35efc07c8303c
horis <horis233@gmail.com>
2019-01-03 12:49:26 -0500

    Add Title component

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import {AppContext} from '../App/AppProvider';
+import {Tile} from '../Shared/Tile';
 
 export const CoinGridStyled = styled.div`
     display: grid;
@@ -10,7 +11,7 @@ export default function (){
     return <AppContext.Consumer>
         {({coinList}) => <CoinGridStyled>
             {Object.keys(coinList).map(coinKey =>
-                <div> {coinKey} </div>    
+                <Tile> {coinKey} </Tile>    
             )}
         </CoinGridStyled>}
     </AppContext.Consumer>
---
+++ src/Shared/Tile.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import {subtleBoxShadow, lightBlueBackground, greenBoxShadow} from './Styles';
+
+export const Tile = styled.div`
+    ${subtleBoxShadow}
+    ${lightBlueBackground}
+    padding: 10px;
+`
