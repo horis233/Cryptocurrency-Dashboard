4d6434962e1d8e258a9caa5492397f6d63c2c410
horis <horis233@gmail.com>
2019-01-03 23:35:54 -0500

    Update Page conponent

--- src/Settings/ConfirmButton.jsx
+++ src/Settings/ConfirmButton.jsx
@@ -1,34 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
-import { AppContext } from '../App/AppProvider';
-import { fontSize1, greenBoxShadow, color3 } from '../Shared/Styles';
+import {fontSize1, greenBoxShadow, color3} from '../Shared/Styles'
+import React from 'react'
+import styled from 'styled-components'
+import {AppContext} from "../App/AppProvider"
 
 const ConfirmButtonStyled = styled.div`
-    margin: 20px;
-    color: ${color3}
-    ${fontSize1}
-    padding: 5px;
+  margin: 20px;
+  color: ${color3}
+  ${fontSize1}
+  font-family: Exo 2, sans-serif;
+  padding: 5px;
+  &:hover {
+    ${greenBoxShadow}
     cursor: pointer;
-    &:hover {
-        ${greenBoxShadow}
-    }
+  }
 `;
 
 export const CenterDiv = styled.div`
-    display: grid;
-    justify-content: center;
+  display: grid;
+  justify-content: center;
 `;
 
 export default function () {
-  return (
-    <AppContext.Consumer>
-      {({ confirmFavorites }) => (
-        <CenterDiv>
-          <ConfirmButtonStyled onClick={confirmFavorites}>
-                    Confirm Favorites
-          </ConfirmButtonStyled>
-        </CenterDiv>
-      )}
-    </AppContext.Consumer>
-  );
+	return (
+		<AppContext.Consumer>
+			{({confirmFavorites}) =>
+				<CenterDiv>
+					<ConfirmButtonStyled onClick={confirmFavorites}>
+            Confirm Favorites
+					</ConfirmButtonStyled>
+				</CenterDiv>
+			}
+		</AppContext.Consumer>
+	)
 }
