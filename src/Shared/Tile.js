ff3a52ee6d3f622628e5c08420cdd83910330e78
horis <horis233@gmail.com>
2019-01-03 23:38:39 -0500

    Update Style

--- src/Shared/Tile.js
+++ src/Shared/Tile.js
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 import { subtleBoxShadow, lightBlueBackground } from './Styles';
 
-const Tile = styled.div`
+export const Tile = styled.div`
     ${subtleBoxShadow}
     ${lightBlueBackground}
     padding: 10px;
 `;
-
-export { Tile as default };
