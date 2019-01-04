526ed625f541f50d87ca2aeb903cc48ac3bcf49c
horis <horis233@gmail.com>
2019-01-04 01:22:49 -0500

    Fix Content

--- src/App/Content.jsx
+++ src/App/Content.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import {AppContext} from "./AppProvider"
 
 export default function (props) {
-  return (
-    <AppContext.Consumer>
-      {({coinList}) => {
+  return <AppContext.Consumer>
+      {(coinList) => {
         if (!coinList) {
           return <div> Loading Coins </div>
         }
         return <div> {props.children}</div>
       }}
     </AppContext.Consumer>
-  )
 }
