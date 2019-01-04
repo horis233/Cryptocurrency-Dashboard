60ac7bc5d33ca158e5ba5b226c489e8d76303ac5
horis <horis233@gmail.com>
2019-01-04 01:29:27 -0500

    Update CoinGrid Style

--- src/Settings/CoinGrid.jsx
+++ src/Settings/CoinGrid.jsx
@@ -4,8 +4,17 @@ import {AppContext} from '../App/AppProvider';
 import {Tile} from '../Shared/Tile';
 
 export const CoinGridStyled = styled.div`
-    display: grid;
-`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  ${props =>
+  props.count &&
+  css`
+      grid-template-columns: repeat(${props.count > 5 ? props.count : 5}, 1fr);
+    `}
+  grid-gap: 15px;
+  margin-top: 40px;
+`;
+
 export default function (){  
     return <AppContext.Consumer>
         {(coinList) => <CoinGridStyled>
