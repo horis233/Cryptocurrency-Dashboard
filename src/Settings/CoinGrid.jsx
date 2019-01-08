6b2fa69a7597fdfcedcd6fe06eed751e338ab451
horis <horis233@gmail.com>
2019-01-08 01:08:48 -0500

    Update vscode setting to format code automatically

--- src/Settings/CoinGrid.jsx
+++ src/Settings/CoinGrid.jsx
@@ -1,50 +1,39 @@
-import React from "react";
-import styled from "styled-components";
-import { AppContext } from "../App/AppProvider";
-import CoinTile from "./CoinTile";
+import React from 'react';
+import styled from 'styled-components';
+import { AppContext } from '../App/AppProvider';
+import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
-  grid-gap: 15px;
-  margin-top: 40px;
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+	grid-gap: 15px;
+	margin-top: 40px;
 `;
 
 function getLowerSectionCoins(coinList, filteredCoins) {
-  return (
-    (filteredCoins && Object.keys(filteredCoins)) ||
-    Object.keys(coinList).slice(0, 100)
-  );
+	return (filteredCoins && Object.keys(filteredCoins)) || Object.keys(coinList).slice(0, 100);
 }
 
 function getCoinsToDisplay(coinList, topSection, favorite, filterCoins) {
-  return topSection ? favorite : getLowerSectionCoins(coinList, filterCoins);
+	return topSection ? favorite : getLowerSectionCoins(coinList, filterCoins);
 }
 
 export default function({ topSection }) {
-  return (
-    <AppContext.Consumer>
-      {({ coinList, favorites, filteredCoins }) => {
-        // console.log(coinList);
-        if (coinList) {
-          return (
-            <CoinGridStyled>
-              {getCoinsToDisplay(
-                coinList,
-                topSection,
-                favorites,
-                filteredCoins
-              ).map(coinKey => (
-                <CoinTile
-                  key={coinKey}
-                  coinKey={coinKey}
-                  topSection={topSection}
-                />
-              ))}
-            </CoinGridStyled>
-          );
-        }
-      }}
-    </AppContext.Consumer>
-  );
+	return (
+		<AppContext.Consumer>
+			{({ coinList, favorites, filteredCoins }) => {
+				// console.log(coinList);
+				if (coinList) {
+					return (
+						<CoinGridStyled>
+							{getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey => (
+								<CoinTile key={coinKey} coinKey={coinKey} topSection={topSection} />
+							))}
+						</CoinGridStyled>
+					);
+				}
+				return <div>Loading</div>;
+			}}
+		</AppContext.Consumer>
+	);
 }
