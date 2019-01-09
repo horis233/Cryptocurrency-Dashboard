3e2eaffa4c1491f5103b0bc7493022dd35c347d7
horis <horis233@gmail.com>
2019-01-09 17:21:47 -0500

    Fix dashboard firstvisit bug

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
-import "./App.css";
-import AppLayout from "./AppLayout";
-import AppBar from "./AppBar";
-import { AppProvider } from "./AppProvider";
-import Settings from "../Settings/index";
-import Content from "./Content";
-import Dashboard from "../Dashboard/index";
+import React, { Component } from 'react';
+import './App.css';
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
+import { AppProvider } from './AppProvider';
+import Settings from '../Settings/index';
+import Content from './Content';
+import Dashboard from '../Dashboard/index';
 
 class App extends Component {
-  render() {
-    return (
-      <AppLayout>
-        <AppProvider>
-          <AppBar />
-          <Content>
-            <Settings />
-            <Dashboard />
-          </Content>
-        </AppProvider>
-      </AppLayout>
-    );
-  }
+	render() {
+		return (
+			<AppLayout>
+				<AppProvider>
+					<AppBar />
+					<Content>
+						<Settings />
+						<Dashboard />
+					</Content>
+				</AppProvider>
+			</AppLayout>
+		);
+	}
 }
 
 export default App;
